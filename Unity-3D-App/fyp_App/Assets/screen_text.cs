aaf4557e06073478b1be2f2c5c30a8ce043f779b
Fahad Sohail <fhd1103@outlook.com>
2020-04-13 02:09:12 +0500

    unity app initial files

---
+++ Unity-3D-App/fyp_App/Assets/screen_text.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class screen_text : MonoBehaviour
+{
+
+
+    public Text sentence;
+
+    // Update is called once per frame
+    void Update()
+    {
+        sentence.text = "Hello World!";
+    }
+}
